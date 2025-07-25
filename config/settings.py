76d5fee8c90a4e13495c9dad6253bf9ead8a8eaf
Krum Yakimov <krum.yakimov@gmail.com>
2025-07-25 11:49:23 +0300

    Refactor(settings): Use pathlib for INPUT_DIR path construction

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-INPUT_DIR = os.path.join(BASE_DIR, "INPUT")
+INPUT_DIR = BASE_DIR / "INPUT"
 
 LOG_DIR = BASE_DIR / os.getenv("LOG_DIR", "logs")
 LOG_TO_CONSOLE = config("LOG_TO_CONSOLE", default=False, cast=bool)
